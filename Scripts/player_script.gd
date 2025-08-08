f4817a7a7c0c0e9ebc228f1a3a31e1cfe85128a9
Klarenz Cobie O. Manrique <klarenzcobie99@gmail.com>
2025-08-08 23:21:37 +0800

    Circle Ewan

--- Scripts/player_script.gd
+++ Scripts/player_script.gd
@@ -1,9 +1,21 @@
 extends CharacterBody2D
 
-func _ready():
-	pass
-	
-# TODO: Make a smooth movement system
-func _physics_process(delta: float) -> void:
-	
-	pass
+@export var center := Vector2(0,0)  # center of the ring
+@export var radius := 300.0 # distance from center
+@export var speed := 2.0 # radians per second
+
+var angle := 0.0 # current angle around circle
+
+#func _input(event: InputEvent) -> void:
+	#pass
+
+func _process(delta):
+	var input_dir = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	angle += speed * input_dir * delta
+	angle = fmod(angle, TAU)  # keep angle in [0, 2π]
+
+	# Update position around circle
+	position = center + Vector2(cos(angle), sin(angle)) * radius
+
+	# Rotate to face direction
+	rotation = angle + PI / 2
