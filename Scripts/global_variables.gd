90fddd5c71f4ff7888646f401de07f6df2bf08da
raquem-annierose <annierosesraquem@iskolarngbayan.pup.edu.ph>
2025-08-11 03:23:17 +0800

    set character as cursor  globally

--- Scripts/global_variables.gd
+++ Scripts/global_variables.gd
@@ -7,6 +7,20 @@ const transition_time: float = 0.5
 var is_looping: bool = false
 var player_stopped: bool = false
 var is_restarting: bool = false
+var custom_cursor = preload("res://Assets/Loopling.png")
+
+func _ready():
+	set_custom_cursor()
+
+func set_custom_cursor():
+	if custom_cursor and custom_cursor is Texture2D:
+		var img = custom_cursor.get_image()
+		if img:
+			var scale_factor = 2.5
+			var new_size = img.get_size() * scale_factor
+			img.resize(new_size.x, new_size.y, Image.INTERPOLATE_LANCZOS)
+			var big_cursor = ImageTexture.create_from_image(img)
+			Input.set_custom_mouse_cursor(big_cursor)
 
 func play_sfx(sfx: Object, direction: Directions):
 	if direction == player_direction.CLOCKWISE:
--- Scripts/main_scene.gd
+++ Scripts/main_scene.gd
@@ -1,33 +1,9 @@
 extends Control
 
-var new_cursor = preload("res://Assets/Loopling.png")
-
-
-func set_custom_cursor():
-	print("new_cursor loaded:", new_cursor)
-	if new_cursor and new_cursor is Texture2D:
-		# Try setting the cursor directly first
-		Input.set_custom_mouse_cursor(new_cursor)
-		print("Set cursor directly.")
-		# Now try resizing
-		var img = new_cursor.get_image()
-		if img:
-			var scale_factor = 2.5
-			var new_size = img.get_size() * scale_factor
-			img.resize(new_size.x, new_size.y, Image.INTERPOLATE_LANCZOS)
-			var big_cursor = ImageTexture.create_from_image(img)
-			Input.set_custom_mouse_cursor(big_cursor)
-			print("Set resized cursor.")
-		else:
-			print("Failed to get image from texture.")
-	else:
-		print("Cursor image not found or not a Texture2D")
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	set_custom_cursor()
-
+	pass
 
 # Called every frame. '_delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
