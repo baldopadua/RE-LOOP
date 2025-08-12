410abc85b470c068787dc1f1b0dbadbbc660110c
raquem-annierose <annierosesraquem@iskolarngbayan.pup.edu.ph>
2025-08-12 16:16:31 +0800

    add sfx in tutorial button in game scene

--- Scripts/ui/game_ui_elements.gd
+++ Scripts/ui/game_ui_elements.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var tutorial_button := $ui_frame/tutorial_button
 @onready var tutorial_scene_packed := preload("res://Scenes/ui/tutorial.tscn")
+@onready var page_turn_sound := $ui_frame/page_turn_sound
 var tutorial_instance: Control = null
 
 # Called when the node enters the scene tree for the first time.
@@ -10,9 +11,11 @@ func _ready() -> void:
 	if tutorial_button:
 		tutorial_button.disabled = false
 		tutorial_button.mouse_filter = Control.MOUSE_FILTER_STOP
-	tutorial_button.connect("pressed", Callable(self, "_on_tutorial_button_pressed"))
+		tutorial_button.connect("pressed", Callable(self, "_on_tutorial_button_pressed"))
 
 func _on_tutorial_button_pressed() -> void:
+	if page_turn_sound:
+		page_turn_sound.play()
 	if tutorial_instance == null:
 		tutorial_instance = tutorial_scene_packed.instantiate()
 		add_child(tutorial_instance)
