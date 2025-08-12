89117b0c29658f8e50ce07a080b3743ee68ed72c
raquem-annierose <annierosesraquem@iskolarngbayan.pup.edu.ph>
2025-08-12 15:02:10 +0800

    add sfx to credits

--- Scripts/ui/credits.gd
+++ Scripts/ui/credits.gd
@@ -4,7 +4,8 @@ extends Control
 @onready var credits_overlay := $credits_overlay
 @onready var close_button := $credits_overlay/close_button
 @onready var credits_animated_icon := $credits_button/credits_animated_icon
-
+@onready var hover_sound := $"../hover_sound"
+@onready var page_turn_sound := $"../page_turn_sound"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,13 +15,13 @@ func _ready() -> void:
 	credits_button.connect("mouse_entered", Callable(self, "_on_credits_button_hovered"))
 	credits_button.connect("mouse_exited", Callable(self, "_on_credits_button_unhovered"))
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
-
 func _on_credits_button_pressed() -> void:
+	if page_turn_sound:
+		page_turn_sound.play()
 	credits_overlay.visible = true
 	var main_scene = get_tree().current_scene
 	if main_scene:
@@ -33,17 +34,25 @@ func _on_credits_button_pressed() -> void:
 	if credits_animated_icon:
 		credits_animated_icon.visible = false
 
-
 func _on_close_button_pressed() -> void:
+	if page_turn_sound:
+		page_turn_sound.play()
+	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
 
 func _on_credits_button_hovered() -> void:
+	if hover_sound:
+		hover_sound.play()
 	if credits_animated_icon:
 		var current_frame = credits_animated_icon.frame
 		credits_animated_icon.play("hover")
 		credits_animated_icon.frame = current_frame
 
 func _on_credits_button_unhovered() -> void:
+	if credits_animated_icon:
+		var current_frame = credits_animated_icon.frame
+		credits_animated_icon.play("default")
+		credits_animated_icon.frame = current_frame
 	if credits_animated_icon:
 		var current_frame = credits_animated_icon.frame
 		credits_animated_icon.play("default")
