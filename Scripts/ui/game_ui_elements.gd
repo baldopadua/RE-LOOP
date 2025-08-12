a65d7136e20903e86bb26f459c2bdf90ec269d2b
raquem-annierose <annierosesraquem@iskolarngbayan.pup.edu.ph>
2025-08-12 16:31:51 +0800

    update game uii elements code

--- Scripts/ui/game_ui_elements.gd
+++ Scripts/ui/game_ui_elements.gd
@@ -5,13 +5,15 @@ extends Control
 @onready var page_turn_sound := $ui_frame/page_turn_sound
 var tutorial_instance: Control = null
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$ui_frame/time_indicator.play()
+	_setup_tutorial_button()
+
+func _setup_tutorial_button() -> void:
 	if tutorial_button:
 		tutorial_button.disabled = false
 		tutorial_button.mouse_filter = Control.MOUSE_FILTER_STOP
-		tutorial_button.connect("pressed", Callable(self, "_on_tutorial_button_pressed"))
+		tutorial_button.pressed.connect(_on_tutorial_button_pressed)
 
 func _on_tutorial_button_pressed() -> void:
 	if page_turn_sound:
@@ -19,11 +21,9 @@ func _on_tutorial_button_pressed() -> void:
 	if tutorial_instance == null:
 		tutorial_instance = tutorial_scene_packed.instantiate()
 		add_child(tutorial_instance)
-		tutorial_instance.z_index = 100  # Ensure it's on top
+		tutorial_instance.z_index = 100
 	else:
 		tutorial_instance.visible = true
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
