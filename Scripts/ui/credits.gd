d897ac846953b58e83810e0e9c1a85c618a19682
raquem-annierose <annierosesraquem@iskolarngbayan.pup.edu.ph>
2025-08-12 14:32:09 +0800

    add credits button functionality

--- Scripts/ui/credits.gd
+++ Scripts/ui/credits.gd
@@ -1,11 +1,33 @@
 extends Control
 
+@onready var credits_button := $credits_button
+@onready var credits_overlay := $credits_overlay
+@onready var close_button := $credits_overlay/close_button
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$credits_button/credits_animated_icon.play() 
+	$credits_button/credits_animated_icon.play()
+	credits_button.connect("pressed", Callable(self, "_on_credits_button_pressed"))
+	close_button.connect("pressed", Callable(self, "_on_close_button_pressed"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_credits_button_pressed() -> void:
+	credits_overlay.visible = true
+	var main_scene = get_tree().current_scene
+	if main_scene:
+		if main_scene.has_node("gametitle"):
+			main_scene.get_node("gametitle").visible = false
+		if main_scene.has_node("start_button"):
+			main_scene.get_node("start_button").visible = false
+		if main_scene.has_node("tutorial_button"):
+			main_scene.get_node("tutorial_button").visible = false
+
+
+func _on_close_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
