70cfcd061d6ea5454fbe410f48abf542fdd3c73d
raquem-annierose <annierosesraquem@iskolarngbayan.pup.edu.ph>
2025-08-13 21:09:05 +0800

    remove music bgm in game scene

--- Scripts/game_scene.gd
+++ Scripts/game_scene.gd
@@ -3,8 +3,6 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if $game_scene_bgm:
-		$game_scene_bgm.play()
 	$CanvasLayer/game_scene_bg.play() 
 	
 
