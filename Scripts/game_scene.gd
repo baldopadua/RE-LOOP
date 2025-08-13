9e43ac3c993717130103ad9a280f4fa831dfc45e
raquem-annierose <annierosesraquem@iskolarngbayan.pup.edu.ph>
2025-08-13 20:56:12 +0800

    add bgm holder to game scene

--- Scripts/game_scene.gd
+++ Scripts/game_scene.gd
@@ -3,7 +3,10 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if $game_scene_bgm:
+		$game_scene_bgm.play()
 	$CanvasLayer/game_scene_bg.play() 
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
