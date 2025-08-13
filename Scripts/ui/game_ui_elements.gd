8a21dda348e1cff8736696a66fa4ab21f756ae59
raquem-annierose <annierosesraquem@iskolarngbayan.pup.edu.ph>
2025-08-14 00:20:35 +0800

    add hint overlay

--- Scripts/ui/game_ui_elements.gd
+++ Scripts/ui/game_ui_elements.gd
@@ -3,16 +3,24 @@ extends Control
 @onready var tutorial_button := $ui_frame/tutorial_button
 @onready var tutorial_scene_packed := preload("res://Scenes/ui/tutorial.tscn")
 @onready var page_turn_sound := $ui_frame/page_turn_sound
+@onready var hint_button := $ui_frame/hint_button
 var tutorial_instance: Control = null
 
 func _ready() -> void:
 	_setup_tutorial_button()
+	_setup_hint_button()
 
 func _setup_tutorial_button() -> void:
 	if tutorial_button:
 		tutorial_button.disabled = false
 		tutorial_button.mouse_filter = Control.MOUSE_FILTER_STOP
 		tutorial_button.pressed.connect(_on_tutorial_button_pressed)
+
+func _setup_hint_button() -> void:
+	if hint_button:
+		hint_button.disabled = false
+		hint_button.mouse_filter = Control.MOUSE_FILTER_STOP
+		hint_button.pressed.connect(_on_hint_button_pressed)
 
 func _on_tutorial_button_pressed() -> void:
 	if page_turn_sound:
@@ -23,6 +31,13 @@ func _on_tutorial_button_pressed() -> void:
 		tutorial_instance.z_index = 100
 	else:
 		tutorial_instance.visible = true
+
+func _on_hint_button_pressed() -> void:
+	var hint_node = $ui_frame.get_node("hint")
+	if page_turn_sound:
+		page_turn_sound.play()
+	if hint_node:
+		hint_node.show_hint_overlay()
 
 func _process(_delta: float) -> void:
 	pass
---
+++ Scripts/ui/hint.gd
@@ -0,0 +1,55 @@
+extends Control
+
+const OVERLAY_FINAL_SCALE = Vector2(1, 1)
+const OVERLAY_START_SCALE = Vector2(0.2, 0.2)
+
+@onready var hint_overlay := $hint_overlay
+@onready var close_button := $close_button
+@onready var page_turn_sound := $hint_overlay/page_turn_sound
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	close_button.connect("pressed", Callable(self, "_on_close_button_pressed"))
+	close_button.z_index = 100
+	hint_overlay.z_index = 99
+	# Start hidden until show_hint_overlay is called
+	hint_overlay.visible = false
+
+func show_hint_overlay():
+	visible = true # Ensure the root node is visible
+	hint_overlay.visible = true
+	hint_overlay.scale = Vector2(1, 1)
+	var parent_size = get_viewport_rect().size
+	var overlay_size = hint_overlay.size
+	var final_pos = (parent_size / 2) - (overlay_size / 2)
+	var start_pos = Vector2(-overlay_size.x, final_pos.y)
+	hint_overlay.position = start_pos
+	var tween = create_tween()
+	tween.tween_property(hint_overlay, "position", final_pos, 0.25)\
+		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_close_button_pressed() -> void:
+	if page_turn_sound:
+		page_turn_sound.play()
+	_fade_out_and_close()
+
+
+func _fade_out_and_close():
+	var parent_size = get_viewport_rect().size
+	var overlay_size = hint_overlay.size
+	var end_pos = Vector2(-overlay_size.x, (parent_size.y / 2) - (overlay_size.y / 2)) # to left
+	var tween = create_tween()
+	tween.tween_property(hint_overlay, "position", end_pos, 0.18)\
+		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+	tween.tween_callback(Callable(self, "_on_fade_out_done"))
+
+
+func _on_fade_out_done():
+	hint_overlay.visible = false
+	visible = false # Hide the root node instead of freeing
+	# queue_free() # Remove this line if you want to reuse the node
