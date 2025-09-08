cf9b7be003d750da7b4fb25990839ad20ce54c8d
raquem-annierose <annierosesraquem@iskolarngbayan.pup.edu.ph>
2025-09-08 11:30:45 +0800

    feat: add game UI elements button connections and handlers in main_scene

--- Scripts/main_scene.gd
+++ Scripts/main_scene.gd
@@ -10,6 +10,7 @@ func _ready():
 	transition_handler.show_main_to_game_transition()
 	_connect_main_menu_buttons()
 	_connect_overlay_close_button()
+	_connect_game_ui_elements_buttons()
 
 func _connect_main_menu_buttons():
 	if ui_handler.ui_layout.has_node("main_menu"):
@@ -29,6 +30,20 @@ func _connect_main_menu_buttons():
 			var credits_btn = main_menu.get_node("credits_button")
 			if not credits_btn.is_connected("pressed", Callable(self, "_on_main_menu_button_pressed")):
 				credits_btn.connect("pressed", Callable(self, "_on_main_menu_button_pressed").bind("credits"))
+
+func _connect_game_ui_elements_buttons():
+	if ui_handler.ui_layout.has_node("game_ui_elements"):
+		var game_ui = ui_handler.ui_layout.get_node("game_ui_elements")
+		# Tutorial Button
+		if game_ui.has_node("tutorial_button"):
+			var tutorial_btn = game_ui.get_node("tutorial_button")
+			if not tutorial_btn.is_connected("pressed", Callable(self, "_on_game_ui_elements_button_pressed")):
+				tutorial_btn.connect("pressed", Callable(self, "_on_game_ui_elements_button_pressed").bind("tutorial"))
+		# Hint Button
+		if game_ui.has_node("hint_button"):
+			var hint_btn = game_ui.get_node("hint_button")
+			if not hint_btn.is_connected("pressed", Callable(self, "_on_game_ui_elements_button_pressed")):
+				hint_btn.connect("pressed", Callable(self, "_on_game_ui_elements_button_pressed").bind("hint"))
 
 func _connect_overlay_close_button():
 	if ui_handler.ui_layout.has_node("overlay"):
@@ -65,6 +80,14 @@ func _on_main_menu_button_pressed(button_type):
 			ui_handler.show_overlay_credits()
 			ui_handler.hide_main_menu()
 			ui_handler.show_overlay_credits()
+
+func _on_game_ui_elements_button_pressed(button_type):
+	if ui_handler.sound_manager:
+		if button_type == "tutorial":
+			ui_handler.sound_manager.play_ui("page_turn")
+			ui_handler.show_overlay_tutorial()
+		elif button_type == "hint":
+			pass
 
 
 
