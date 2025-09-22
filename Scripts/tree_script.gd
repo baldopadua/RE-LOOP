25ffb835eba1ae5bf61072ccc147a31300edf293
De Padua, Charles Jeramy C. <cjdp.pogi@gmail.com>
2025-09-22 21:34:38 +0800

    Jump animation Level 1

--- Scripts/tree_script.gd
+++ Scripts/tree_script.gd
@@ -8,6 +8,7 @@ var tween_scale: Tween
 var level_2 = preload("res://Scenes/levels/level_2_scene.tscn")
 var time_indicator: AnimatedSprite2D
 var is_playing: bool = false
+var player_body: Node 
 
 # HANDLERS
 @onready var sound_manager = $SoundManager
@@ -26,6 +27,9 @@ func _on_body_entered(body) -> void:
 		is_playing = true
 		# DISABLE PLAYER MOVEMENT
 		GlobalVariables.player_stopped = true
+		
+		# STORE THE PLAYER REFERENCE
+		player_body = body
 			
 		await get_tree().create_timer(1).timeout
 		
@@ -34,26 +38,30 @@ func _on_body_entered(body) -> void:
 		time_indicator.frame = 0
 		time_indicator.pause()
 		
-		# PLAY CLIMB ANIMATION
+		# PLAY CLIMB ANIMATION ON PLAYER SPRITE
 		if body.has_node("AnimatedSprite2D"):
 			var sprite = body.get_node("AnimatedSprite2D")
 			sprite.stop()
 			sprite.play("climb")
 		
 		# CLIMB
-		
-		# 1. REMOVE TWEEN 
-	
 		sound_manager.play_player_sfx("Climb")
 		anim_handler.play("ClimbingAnimation")
-		
-		# Wait for climbing animation to be finished
 
 func _tween_climb_finished():
 	tween_climb.kill()
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "ClimbingAnimation":
+		# Play jump animation on player sprite after climbing finishes
+		if player_body and player_body.has_node("AnimatedSprite2D"):
+			var sprite = player_body.get_node("AnimatedSprite2D")
+			sprite.stop()
+			sprite.play("jump")
+		
+		# Play jump animation in AnimationPlayer
+		anim_handler.play("JumpAnimation")
+	elif anim_name == "JumpAnimation":
 		# DECLARE LEVEL TO BE FINISHED
 		var level_1 = get_parent()
 		level_handler.next_level(level_1, tween_rotate, tween_scale, "res://Scenes/levels/level_2_scene.tscn")
