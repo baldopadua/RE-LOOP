88c7dcae5488361e90bb2fa68c0c623936e02ede
raquem-annierose <annierosesraquem@iskolarngbayan.edu.ph>
2025-09-25 01:18:05 +0800

    rename layout as logic

--- Scripts/component/transition_handler.gd
+++ Scripts/component/transition_handler.gd
@@ -1,10 +1,10 @@
 extends Control
 
-var layout: Control
+var logic: Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	layout = get_node("transition_layout")
+	logic = get_node("transition_logic")
 
 func show_background() -> void:
 	var bg = get_node("background")
@@ -15,15 +15,9 @@ func show_background() -> void:
 			anim.frame = 0
 			anim.play("default")
 			
-# func remove_transition(transition_name: String) -> void:
-# 	if layout and layout.has_node(transition_name):
-# 		var node = layout.get_node(transition_name)
-# 		node.visible = false
-# 		node.queue_free()
-
 func show_child_node(node_name: String) -> void:
-	if layout and layout.has_node(node_name):
-		var node = layout.get_node(node_name)
+	if logic and logic.has_node(node_name):
+		var node = logic.get_node(node_name)
 		_show_node_and_children(node)
 
 func _show_node_and_children(node: Node) -> void:
@@ -34,5 +28,5 @@ func _show_node_and_children(node: Node) -> void:
 func show_main_to_game_transition() -> void:
 	show_background()
 	show_child_node("main_to_game")
-	if layout:
-		layout.play_plooy_falling_animation()
+	if logic:
+		logic.play_plooy_falling_animation()
