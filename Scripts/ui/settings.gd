8fe5da16017486d3c6ecf088121d081760fb6508
raquem-annierose <annierosesraquem@iskolarngbayan.edu.ph>
2025-11-15 16:51:37 +0800

    add mute in volume

--- Scripts/ui/settings.gd
+++ Scripts/ui/settings.gd
@@ -3,6 +3,9 @@ extends Control
 var sfx_volume_label: Label
 var music_volume_label: Label
 
+var last_sfx_volume: float = 100
+var last_music_volume: float = 100
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -27,7 +30,13 @@ func _on_music_slider_value_changed(value: float) -> void:
 		var sound_manager = get_tree().get_root().find_child("SoundManager", true, false)
 		if sound_manager:
 			sound_manager.set_music_bus_volume(value)
-
+		# If not muted, update last_music_volume
+		if value > 0:
+			last_music_volume = value
+		# Update mute button texture state
+		var mute_btn = $settings_box/music_mute_button
+		if mute_btn:
+			mute_btn.set_pressed(value == 0)
 
 func _on_sfx_slider_value_changed(value: float) -> void:
 	if sfx_volume_label:
@@ -35,3 +44,39 @@ func _on_sfx_slider_value_changed(value: float) -> void:
 		var sound_manager = get_tree().get_root().find_child("SoundManager", true, false)
 		if sound_manager:
 			sound_manager.set_sfx_bus_volume(value)
+		# If not muted, update last_sfx_volume
+		if value > 0:
+			last_sfx_volume = value
+		# Update mute button texture state
+		var mute_btn = $settings_box/sfx_mute_button
+		if mute_btn:
+			mute_btn.set_pressed(value == 0)
+
+
+
+func _on_music_mute_button_pressed() -> void:
+	var slider = $settings_box/music_slider
+	var sound_manager = get_tree().get_root().find_child("SoundManager", true, false)
+	if slider.value > 0:
+		last_music_volume = slider.value
+		slider.value = 0
+	else:
+		slider.value = last_music_volume
+	# Update label and bus volume
+	music_volume_label.text = str(int(slider.value)) + "%"
+	if sound_manager:
+		sound_manager.set_music_bus_volume(slider.value)
+
+
+func _on_sfx_mute_button_pressed() -> void:
+	var slider = $settings_box/sfx_slider
+	var sound_manager = get_tree().get_root().find_child("SoundManager", true, false)
+	if slider.value > 0:
+		last_sfx_volume = slider.value
+		slider.value = 0
+	else:
+		slider.value = last_sfx_volume
+	# Update label and bus volume
+	sfx_volume_label.text = str(int(slider.value)) + "%"
+	if sound_manager:
+		sound_manager.set_sfx_bus_volume(slider.value)
