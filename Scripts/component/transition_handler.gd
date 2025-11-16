cb15ae3e46c5ece343b7f8ff5d06c62973c09245
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2025-11-16 16:04:45 +0800

    remove sfx in transition_handler.gd (mistake)

--- Scripts/component/transition_handler.gd
+++ Scripts/component/transition_handler.gd
@@ -28,8 +28,5 @@ func _show_node_and_children(node: Node) -> void:
 func show_main_to_game_transition() -> void:
 	show_background()
 	show_child_node("main_to_game")
-	# Play wormhole sound
-	if sound_manager:
-		sound_manager.play_sfx("Climb")
 	if logic:
 		logic.play_plooy_falling_animation()
