6119f7ab23aa5ade760bfda9982b71b16d8e30a6
Klarenz Cobie O. Manrique <klarenzcobie99@gmail.com>
2025-11-16 23:29:58 +0800

    Add level 11 object scripts and update level scenes
    Introduced new scripts for level 11 objects (apple, butterfly, carrot, cat, egg, lightbulb, player, plooy_statue, rock, seed, skull, soda) to handle drop logic and matching. Updated level_11_scene.tscn to assign these scripts, connect signals, and remove grayscale shader usage from keystone objects. Minor adjustments to cinematic bar scaling in multiple level scenes and added new UI dialog assets. Also updated level_11_script.gd to integrate the new dialogue balloon and object logic.
    
    Note: level_11 is still unpolished and unfinished, especially the dialogue balloons.

--- Scripts/levels/level_6_script.gd
+++ Scripts/levels/level_6_script.gd
@@ -133,6 +133,3 @@ func enable_animation_player():
 func play_climb_animation(body: Node) -> void:
 	self.player_body = body
 	anim_handler.play("climbing_animation")
-
-
-
---
+++ Scripts/object-level/level_11/cat.gd
@@ -0,0 +1,10 @@
+extends object_class
+var matched = false
+@onready var animated_sprite2 = $AnimatedSprite2D2
+func _on_is_dropped(plooy_rotation: Variant) -> void:
+	if round(position) == Vector2(-230.0, 0.0):
+		pulse("green", animated_sprite2)
+		matched = true
+	else:
+		pulse("red", animated_sprite2)
+		matched = false
