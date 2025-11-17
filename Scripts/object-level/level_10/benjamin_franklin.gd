d10b0d4105cbc2816ac2b7a6cf15a1a10fec0640
De Padua, Charles Jeramy C. <cjdp.pogi@gmail.com>
2025-11-17 23:10:19 +0800

    Level 10
    No Loop Break

--- Scripts/object-level/level_10/benjamin_franklin.gd
+++ Scripts/object-level/level_10/benjamin_franklin.gd
@@ -3,11 +3,12 @@ extends object_class
 @warning_ignore("unused_signal")
 signal add_cur_state(direction)
 
-# INITIALLY SET TO TRUE AS THERE ARE NO WAY TO CURRENTLY KNOW THE CURRENT AREAS OBJECTS WITHIN IT
 var is_in_another_object = true
 var object_that_this_is_on = "light_bulb"
+
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var laser = $"../Laser"
+@onready var lightning_cloud = $"../LightningCloud"
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.object_type == GlobalVariables.object_types.NONTOOL:
@@ -35,25 +36,26 @@ func _on_add_cur_state(direction: Variant) -> void:
 			elif current_state == 2:
 				animated_sprite.play_backwards("ben_f_skull")
 				is_pickupable = true
+
 	elif object_that_this_is_on == "lightning_cloud":
 		if direction == GlobalVariables.player_direction.CLOCKWISE:
 			if current_state == 1:
 				animated_sprite.play("default")
 				is_pickupable = false
 			elif current_state == 2:
-				animated_sprite.play("ben_f_invented")
+				lightning_cloud.get_node("AnimatedSprite2D").play("lightning_rod")
 				is_pickupable = false
-				# SET ONE OF LASER'S KEYSTONE TO SUCCESS
 				laser.emit_signal("keystone_complete", object_name, true)
 		else:
 			if current_state == 1:
 				animated_sprite.play_backwards("default")
 				is_pickupable = false
-				# UNSET ONE OF LASER'S KEYSTONE TO SUCCESS
 				laser.emit_signal("keystone_complete", object_name, false)
 			elif current_state == 2:
-				animated_sprite.play_backwards("ben_f_invented")
+				# reverse not needed for lightning rod, so do nothing or stop it
+				lightning_cloud.get_node("AnimatedSprite2D").play_backwards("lightning_rod")
 				is_pickupable = false
+
 	elif object_that_this_is_on == "light_bulb":
 		if direction == GlobalVariables.player_direction.CLOCKWISE:
 			if current_state == 1:
@@ -69,6 +71,7 @@ func _on_add_cur_state(direction: Variant) -> void:
 			elif current_state == 2:
 				animated_sprite.play_backwards("ben_f_skull")
 				is_pickupable = true
+
 	elif object_that_this_is_on == "tesla_coil":
 		if direction == GlobalVariables.player_direction.CLOCKWISE:
 			if current_state == 1:
